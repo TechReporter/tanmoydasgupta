1a673c924f26e93a5d329cddeefc593bd56c80e5
Tanmoy Dasgupta <tanmoy.007ece@gmail.com>
2019-08-12 03:28:30 +0530

    commit portfolio files changes

--- javascript/home.js
+++
@@ -1,8 +0,0 @@
-function periodical() {
-	$('#exploreMe').effect('shake', { times:2 }, 300);
-}
-$(document).ready(function() {
-	$('#exploreMe').hide().css('display','').fadeIn(100);
-    setInterval(periodical, 5000);
-		
-});
